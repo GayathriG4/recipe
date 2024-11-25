a7a1f345bed59a3e032d41f3e5278fca2386633a
GayathriG4 <gayathriarumugam284@gmail.com>
2024-11-26 02:43:01 +0530

    added

--- app.js
+++ app.js
@@ -7,4 +7,9 @@ app.use(express.json());
 
 app.use('/api/v1/recipes', recipeRouter);
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the Recipe API. Use /api/v1/recipes for CRUD operations.');
+});
+
+
 module.exports = app;
